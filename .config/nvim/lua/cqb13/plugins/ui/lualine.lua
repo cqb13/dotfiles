1c8ee73c6b44f450815c3cf261a2023f1355e250
cqb13 <cqb13.main@gmail.com>
2025-01-05 16:53:29 -0500

    lualine updates

--- .config/nvim/lua/cqb13/plugins/ui/lualine.lua
+++ .config/nvim/lua/cqb13/plugins/ui/lualine.lua
@@ -6,39 +6,39 @@ return {
 		local lazy_status = require("lazy.status")
 
 		local colors = {
-			blue = "#65D1FF",
-			green = "#3EFFDC",
-			violet = "#FF61EF",
-			yellow = "#FFDA7B",
-			red = "#FF4A4A",
+			blue = "#89b4fa",
+			green = "#a6e3a1",
+			violet = "#cba6f7",
+			yellow = "#f9e2af",
+			red = "#f38ba8",
 			fg = "#c3ccdc",
-			bg = "#2c3043",
-			inactive_bg = "#181825",
+			dark_fg = "1e1e2e",
+			bg = "#00000000",
 		}
 
 		local my_lualine_theme = {
 			normal = {
-				a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+				a = { bg = colors.blue, fg = colors.dark_fg, gui = "bold" },
 				b = { bg = colors.bg, fg = colors.fg },
 				c = { bg = colors.bg, fg = colors.fg },
 			},
 			insert = {
-				a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+				a = { bg = colors.green, fg = colors.dark_fg, gui = "bold" },
 				b = { bg = colors.bg, fg = colors.fg },
 				c = { bg = colors.bg, fg = colors.fg },
 			},
 			visual = {
-				a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+				a = { bg = colors.violet, fg = colors.dark_fg, gui = "bold" },
 				b = { bg = colors.bg, fg = colors.fg },
 				c = { bg = colors.bg, fg = colors.fg },
 			},
 			command = {
-				a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+				a = { bg = colors.yellow, fg = colors.dark_fg, gui = "bold" },
 				b = { bg = colors.bg, fg = colors.fg },
 				c = { bg = colors.bg, fg = colors.fg },
 			},
 			replace = {
-				a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+				a = { bg = colors.red, fg = colors.dark_fg, gui = "bold" },
 				b = { bg = colors.bg, fg = colors.fg },
 				c = { bg = colors.bg, fg = colors.fg },
 			},
