b14108d656fbb4b00dc06d8f6686ef7ed50652c2
cqb13 <cqb13.main@gmail.com>
2025-04-04 10:08:20 -0400

    updated c main snippet and removed c print snippet

--- .config/nvim/snippets/c.lua
+++ .config/nvim/snippets/c.lua
@@ -1,6 +1,5 @@
 local ls = require("luasnip")
 local s = ls.s
-local i = ls.i
 
 local fmt = require("luasnip.extras.fmt").fmt
 
@@ -9,25 +8,16 @@ local snippets, autosnippets = {}, {}
 local boiler = fmt(
 	[[
 	#include <stdio.h>
+  #include <stdio.h>
 
-  int main() {{
-    printf("{}\n");
+  int main(int argc, char *argv[]) {{
     return 0;
   }}
   ]],
-	{ i(1) }
-)
-
-local print = fmt(
-	[[
-  printf("{}\n");
-  ]],
-	{ i(1) }
+	{}
 )
 
 local boiler_snippet = s("!main", boiler)
-local print_snippet = s("print", print)
 table.insert(snippets, boiler_snippet)
-table.insert(snippets, print_snippet)
 
 return snippets, autosnippets
