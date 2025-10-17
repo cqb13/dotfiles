10144d59beac7592393320c082f1bffbfb240d1c
cqb13 <cqb13.main@gmail.com>
2025-10-16 21:33:13 -0400

    changed tab amount

--- .config/nvim/lua/cqb13/core/options.lua
+++ .config/nvim/lua/cqb13/core/options.lua
@@ -8,8 +8,8 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.shiftwidth = 4
 opt.smarttab = true
 opt.expandtab = true -- expand tab to space
 opt.autoindent = true -- copy indent from current line when starting a new one
