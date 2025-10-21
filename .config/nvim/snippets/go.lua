d0503382ddce48c897e2b20062eb29d6e0339dc5
cqb13 <cqb13.main@gmail.com>
2025-10-21 13:44:06 -0400

    added more completions for go

--- .config/nvim/snippets/go.lua
+++ .config/nvim/snippets/go.lua
@@ -1,5 +1,6 @@
 local ls = require("luasnip")
 local s = ls.s
+local i = ls.insert_node
 
 local fmt = require("luasnip.extras.fmt").fmt
 
@@ -29,9 +30,36 @@ local error_handle = fmt(
 	{}
 )
 
+local printf = fmt(
+    [[
+    fmt.Printf({})
+    ]],
+    {i(1)}
+)
+
+local println = fmt(
+    [[
+    fmt.Println({})
+    ]],
+    {i(1)}
+)
+
+local errorf = fmt(
+    [[
+    fmt.Errorf({})
+    ]],
+    {i(1)}
+)
+
 local boiler_snippet = s("!main", boiler)
 local error_handle_snippet = s("!err", error_handle)
+local printf_snippet = s("printf", printf)
+local println_snippet = s("println", println)
+local errorf_snippet = s("errorf", errorf)
 table.insert(snippets, boiler_snippet)
 table.insert(snippets, error_handle_snippet)
+table.insert(snippets, printf_snippet)
+table.insert(snippets, println_snippet)
+table.insert(snippets, errorf_snippet)
 
 return snippets, autosnippets
