bf7a66cc9a4b929ce2c76012e2f0c8fa935a1849
cqb13 <cqb13.main@gmail.com>
2026-01-21 10:49:38 -0500

    added cmd to save without formatting

--- .config/nvim/lua/cqb13/plugins/tools/formatting.lua
+++ .config/nvim/lua/cqb13/plugins/tools/formatting.lua
@@ -2,41 +2,52 @@ return {
 	"stevearc/conform.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		local conform = require("conform")
+	local conform = require("conform")
 
-		conform.setup({
-			formatters = {
-				stylua = {
-					args = { "--respect-editorconfig", "-" },
-				},
+	vim.api.nvim_create_user_command("S", function()
+		vim.b.disable_autoformat = true
+		vim.cmd("write")
+		vim.b.disable_autoformat = false
+	end, {})
+
+	conform.setup({
+		formatters = {
+			stylua = {
+				args = { "--respect-editorconfig", "-" },
 			},
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				lua = { "stylua" },
-				python = { "black", "isort" },
-                c = {"clang-format"},
-			},
-			format_on_save = {
+		},
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			lua = { "stylua" },
+			python = { "black", "isort" },
+			c = { "clang-format" },
+		},
+		format_on_save = function(bufnr)
+			if vim.b[bufnr].disable_autoformat then
+				return
+			end
+
+			return {
 				lsp_fallback = true,
 				async = false,
 				timeout_ms = 1000,
-			},
+			}
+		end,
+	})
+	vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+		conform.format({
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 1000,
 		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
+	end, { desc = "Format file or range (in visual mode)" })
 	end,
 }
